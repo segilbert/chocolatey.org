b02e91bf1d6c7659b354d4e293f80e2cd00a91d5
Rob Reynolds <ferventcoder@gmail.com>
2012-01-28 16:45:08 -0600

    Making the formatting better on the RSS Feed...

--- chocolatey/Website/Services/RssActionResult.cs
+++ chocolatey/Website/Services/RssActionResult.cs
@@ -12,11 +12,15 @@
         {
             context.HttpContext.Response.ContentType = "application/rss+xml";
 
-            Rss20FeedFormatter rssFormatter = new Rss20FeedFormatter(Feed);
-
-            using (XmlWriter writer = XmlWriter.Create(context.HttpContext.Response.Output))
+            if (Feed != null)
             {
-                rssFormatter.WriteTo(writer);
+                Rss20FeedFormatter rssFormatter = new Rss20FeedFormatter(Feed);
+
+                using (var xmlWriter = new XmlTextWriter(context.HttpContext.Response.Output))
+                {
+                    xmlWriter.Formatting = Formatting.Indented;
+                    rssFormatter.WriteTo(xmlWriter);
+                }
             }
         }
     }
