4c07e3229db0141af5ee51849d398ef4261ebe56
Rob Reynolds <ferventcoder@gmail.com>
2012-12-10 21:52:57 -0600

    Moving pagescontroller

---
+++ chocolatey/Website/Controllers/PagesController.cs
@@ -0,0 +1,48 @@
+﻿using System.Web.Mvc;
+using System.Web.UI;
+
+namespace NuGetGallery
+{
+    public partial class PagesController : Controller
+    {
+        private readonly IAggregateStatsService statsSvc;
+
+        public PagesController(IAggregateStatsService statsSvc)
+        {
+            this.statsSvc = statsSvc;
+        }
+
+        public virtual ActionResult Home()
+        {
+            return View();
+        }
+
+        public virtual ActionResult Terms()
+        {
+            return View();
+        }
+
+        public virtual ActionResult Privacy()
+        {
+            return View();
+        }
+
+        //public ActionResult Install()
+        //{
+        //    return File(Url.Content("~/installChocolatey.ps1"), "text/plain");
+        //}
+
+        [HttpGet]
+        [OutputCache(VaryByParam = "None", Duration = 120, Location = OutputCacheLocation.Server)]
+        public virtual JsonResult Stats()
+        {
+            var stats = statsSvc.GetAggregateStats();
+            return Json(new
+            {
+                Downloads = stats.Downloads.ToString("n0"),
+                UniquePackages = stats.UniquePackages.ToString("n0"),
+                TotalPackages = stats.TotalPackages.ToString("n0")
+            }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
